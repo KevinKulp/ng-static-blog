e5bbfe8f202ff57aabcc003a8fb68fc0afdfcd06
Kevin Kulp <musicthreads@hotmail.com>
2023-02-25 15:59:29 -0500

    removed api and updated service to use mock data

--- apps/api/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  db_host: 'hostname',
-  db_port: 1234,
-  db_username: 'username',
-  db_password: 'password',
-  db_database: 'dbname',
-};
--- apps/ng-blog/src/app/store/post/post.service.ts
+++ apps/ng-blog/src/app/store/post/post.service.ts
@@ -1,19 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, Observable } from 'rxjs';
+import { mockPost } from '@ng-blog/domain';
+import { find, from, Observable, of, switchMap } from 'rxjs';
 import type { Post } from '@ng-blog/domain';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  readonly posts: Post[] = [
+    mockPost(),
+    mockPost(),
+    mockPost(),
+    mockPost(),
+    mockPost(),
+    mockPost(),
+    mockPost(),
+  ]
+
   constructor(private http: HttpClient) { }
 
   loadPosts(): Observable<{ posts: Post[] }> {
-    return this.http.get<{ posts: [] }>('/api/post').pipe(delay(2000));
+    return of({ posts: this.posts });
+    //return this.http.get<{ posts: [] }>('/api/post').pipe(delay(2000));
   }
 
   loadPost(permalink: string): Observable<Post> {
-    return this.http.get<Post>(`/api/post/${permalink}`).pipe(delay(2000));
+    return of({ posts: this.posts })
+      .pipe(switchMap(
+        data => from(data.posts)),
+        find((post) => post.permalink === permalink)
+      );
+    //return this.http.get<Post>(`/api/post/${permalink}`).pipe(delay(2000));
   }
 }
